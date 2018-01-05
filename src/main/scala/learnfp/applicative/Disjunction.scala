452168261ada906204a54c653cf518d478434eb0
张志豪 <izhangzhihao@hotmail.com>
2018-01-05 18:18:21 +0800

    Implement `Applicative`

--- src/main/scala/learnfp/applicative/Disjunction.scala
+++ src/main/scala/learnfp/applicative/Disjunction.scala
@@ -5,8 +5,11 @@ import learnfp.functor.DisjunctionInstance._
 
 object DisjunctionInstance {
   implicit def disjunctionInstance[L] = new Applicative[({type E[X] = Disjunction[L, X]})#E]() {
-    override def pure[A](a: A): Disjunction[L, A] = ???
-    override def <*>[A, R](dfx: Disjunction[L, A => R])(da: Disjunction[L, A]): Disjunction[L, R] = ???
+    override def pure[A](a: A): Disjunction[L, A] = right(a)
+    override def <*>[A, R](dfx: Disjunction[L, A => R])(da: Disjunction[L, A]): Disjunction[L, R] = dfx match {
+      case RightDisjunction(fn) => da fmap fn
+      case LeftDisjunction(lv) => LeftDisjunction[L,R](lv)
+    }
   }
 
   implicit def disjunctionToApplicativeOps[L, A, R](fx:Disjunction[L, A => R])(
