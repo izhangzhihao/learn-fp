c156816efbaeb7adf2af4e9aa743661079d1f722
张志豪 <izhangzhihao@hotmail.com>
2018-01-05 17:39:27 +0800

    Implement `Monad` part

--- src/main/scala/learnfp/monad/Disjunction.scala
+++ src/main/scala/learnfp/monad/Disjunction.scala
@@ -5,8 +5,11 @@ import learnfp.functor.DisjunctionInstance._
 
 object DisjunctionInstance {
   implicit def disjunctionMonadInstance[L] = new Monad[({type E[A] = Disjunction[L, A]})#E]() {
-    override def pure[A](a: A): Disjunction[L, A] = ???
-    override def flatMap[A, B](a: Disjunction[L, A])(fx: A => Disjunction[L, B]): Disjunction[L, B] = ???
+    override def pure[A](a: A): Disjunction[L, A] = right(a)
+    override def flatMap[A, B](a: Disjunction[L, A])(fx: A => Disjunction[L, B]): Disjunction[L, B] = a match {
+      case at:RightDisjunction[L,A] => fx(at.rightValue)
+      case at:LeftDisjunction[L,A] => left(at.leftValue)
+    }
   }
 
   implicit def disjunctionToMonadOps[L, R](disjunction:Disjunction[L, R]) = new MonadOps[R, ({type E[X] = Disjunction[L, X]})#E](disjunction)
--- src/main/scala/learnfp/monad/State.scala
+++ src/main/scala/learnfp/monad/State.scala
@@ -5,8 +5,11 @@ import learnfp.functor.StateInstance._
 
 object StateInstance {
   implicit def stateMonadInstance[S] = new Monad[({type E[X] = State[S, X]})#E]() {
-    override def pure[A](a: A): State[S, A] = ???
-    override def flatMap[A, B](a: State[S, A])(fx: A => State[S, B]): State[S, B] = ???
+    override def pure[A](a: A): State[S, A] = State(s => (s,a))
+    override def flatMap[A, B](a: State[S, A])(fx: A => State[S, B]): State[S, B] = State{ s =>
+      val (as,aa) = a.run(s)
+      fx(aa).run(as)
+    }
   }
 
   implicit def stateToMonadOps[S, A](a:State[S, A]) = new MonadOps[A, ({type E[X] = State[S, X]})#E](a)
